9e18495630f295fe79624259139d85adf1c258c9
umarvortex <umarvortex@gmail.com>
2025-02-22 05:25:00 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick= () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll= () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+
+
+
+
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Anywhere in Karachi', 'No.1 in Karachi', 'Fast and Secure Dilivery'],
+    typeSpeed: 60,
+    backSpeed: 60,
+    backDelay: 1000,
+    loop: true,
+});
