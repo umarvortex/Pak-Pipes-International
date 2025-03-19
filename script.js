8e6c7c5ea9e523c4b78a73791e1154bcc8563464
Pak Pipes international <umarvortex@gmail.com>
2025-03-19 11:33:53 +0500

    Update script.js

--- script.js
+++ script.js
@@ -39,8 +39,8 @@ const typed = new Typed('.multiple-text', {
         'Trusted by Homeowners and Builders', 
         'Eco-Friendly Pipes',],
     typeSpeed: 60,
-    backSpeed: 2,
-    backDelay: 1050,
+    backSpeed: 0.1,
+    backDelay: 1070,
     loop: true,
 });
 
