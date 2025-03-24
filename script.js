0d62bfa4429730df1bd738af1178e0228d83f116
Pak Pipes international <umarvortex@gmail.com>
2025-03-24 17:42:13 +0500

    Update script.js

--- script.js
+++ script.js
@@ -48,11 +48,14 @@ const typed = new Typed('.multiple-text', {
 
 
 
-   
-
-
         
+function openChatbot() {
+  document.getElementById('chatbot-container').style.display = 'block';
+}
 
+function closeChatbot() {
+  document.getElementById('chatbot-container').style.display = 'none';
+}
 
 
 
